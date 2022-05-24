f0d49c9ce5bd634f2f65aa1f66073630f2650189
YetiFrozty <nope>
2022-05-24 23:53:21 +0200

    made install script more clear

--- install.sh
+++ install.sh
@@ -1,10 +1,15 @@
+#stow the dotfiles
 cd ~/.dotfiles
 mkdir ~/.config
-mkdir ~/.config/nitrogen
 stow .
+
+#install wallpapers
+mkdir ~/.config/nitrogen
 cd ~/.config/nitrogen
 git clone https://github.com/thefallnn/Nord-Wallpapers
 git clone https://github.com/linuxdotexe/nordic-wallpapers
+echo "Dont forget to add the wallpaper folder in nitrogen later!"
+
+#install vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-echo "Dont forget to add the wallpaper folder in nitrogen later!"
