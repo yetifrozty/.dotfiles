01a5c13ffab19d5b06b9fe006d0f3d4875e0d308
YetiFrozty <nope>
2022-05-24 23:46:32 +0200

    added vim plug to install script

--- install.sh
+++ install.sh
@@ -5,4 +5,6 @@ stow .
 cd ~/.config/nitrogen
 git clone https://github.com/thefallnn/Nord-Wallpapers
 git clone https://github.com/linuxdotexe/nordic-wallpapers
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 echo "Dont forget to add the wallpaper folder in nitrogen later!"
