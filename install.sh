afb116d798f36dc77f4700df53a2073eca2eae6b
YetiFrozty <nope>
2022-05-24 23:35:06 +0200

    added another nord wallpaper repo

--- install.sh
+++ install.sh
@@ -4,4 +4,5 @@ mkdir ~/.config/nitrogen
 stow .
 cd ~/.config/nitrogen
 git clone https://github.com/thefallnn/Nord-Wallpapers
+git clone https://github.com/linuxdotexe/nordic-wallpapers
 echo "Dont forget to add the wallpaper folder in nitrogen later!"
