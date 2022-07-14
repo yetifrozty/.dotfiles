71053ad3355cf749bf40e59617d8e4c17fa62d07
YetiFrozty <nope>
2022-07-14 15:11:06 +0200

    added fonts to install script

--- install.sh
+++ install.sh
@@ -18,3 +18,22 @@ echo "Dont forget to add the wallpaper folder in nitrogen later!"
 #install vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+#install the required fonts
+mkdir -p ~/.local/share/fonts
+mkdir ~/.dotfiles/fonts
+cd ~/.dotfiles/fonts
+wget https://download.jetbrains.com/fonts/JetBrainsMono-2.242.zip
+unzip JetBrainsMono-2.242.zip
+mv fonts/ttf/* ~/.local/share/fonts
+
+cd ~/.local/share/fonts
+wget https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/DejaVuSansMono/Regular/complete/DejaVu%20Sans%20Mono%20Nerd%20Font%20Complete.ttf?raw=true
+wget https://github.com/ryanoasis/nerd-fonts/blob/master/src/glyphs/Symbols-1000-em%20Nerd%20Font%20Complete.ttf?raw=true
+
+cd ~/.dotfiles
+rm -rf fonts
+fc-cache -f -v
+
+#go back to the dotfiles directory
+cd ~/.dotfiles
